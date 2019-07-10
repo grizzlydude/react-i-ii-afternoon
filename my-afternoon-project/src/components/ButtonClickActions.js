07dcde2f1e16ac9527e2cd21685d050dc82779dd
Grant <gwensignak@gmail.com>
2019-07-10 10:46:19 -0600

    Working on Previous function

---
+++ my-afternoon-project/src/components/ButtonClickActions.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class ButtonClickActions extends Component{
+    constructor(){
+        super()
+    }
+
+    
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
