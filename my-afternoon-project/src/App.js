bcf04bbd0bce23f596c1e8aaeb354726471faffd
Grant <gwensignak@gmail.com>
2019-07-10 11:13:50 -0600

    Previous Fucntion works

--- my-afternoon-project/src/App.js
+++ my-afternoon-project/src/App.js
@@ -9,82 +9,85 @@ export default class App extends Component {
     this.state = {
       dataDisplayIndex: 0
     }
+    this.handlePrevious = this.handlePrevious.bind(this)
   }
 
   handlePrevious() {
-    this.setState({
-      if(this.dataDisplayIndex < 0) {
-      dataDisplayIndex: dataRedo.length - 1
-    }
+    if (this.state.dataDisplayIndex === 0) {
+      this.setState({
+        dataDisplayIndex: data.length - 1
+      })
+  }
     else {
-      dataDisplayIndex: dataDisplayIndex - 1
+      this.setState({
+        dataDisplayIndex: this.state.dataDisplayIndex - 1
+      })
     }
-  })
-  console.log(this.state.dataDisplayIndex)
-}
+    console.log(this.state.dataDisplayIndex)
+  }
 
 
-// handleNext() {
+  // handleNext() {
 
-// }
+  // }
 
-render() {
-  console.log(data)
-  let dataRedo = data.map(element => {
+  render() {
+    console.log(data)
+    let dataRedo = data.map(element => {
+      return (
+        <div>
+          <h1>
+            First: {element.name.first}
+          </h1>
+          <h1>
+            Last: {element.name.last}
+          </h1>
+          <h1>
+            City: {element.city}
+          </h1>
+          <h1>
+            Country: {element.country}
+          </h1>
+          <h1>
+            Employer: {element.employer}
+          </h1>
+          <h1>
+            Title: {element.title}
+          </h1>
+          <h1>
+            Favorite Movie: {element.favoriteMovies}
+          </h1>
+        </div>
+      )
+    })
+
     return (
-      <div>
-        <h1>
-          First: {element.name.first}
-        </h1>
-        <h1>
-          Last: {element.name.last}
-        </h1>
-        <h1>
-          City: {element.city}
-        </h1>
-        <h1>
-          Country: {element.country}
-        </h1>
-        <h1>
-          Employer: {element.employer}
-        </h1>
-        <h1>
-          Title: {element.title}
-        </h1>
-        <h1>
-          Favorite Movie: {element.favoriteMovies}
-        </h1>
-      </div>
-    )
-  })
+      <div className="App">
+        <div className="blueHeader">
+          <div className="Home">Home</div>
+        </div>
 
-  return (
-    <div className="App">
-      <div className="blueHeader">
-        <div className="Home">Home</div>
-      </div>
+        <div>
+          <div className="blackBody">
 
-      <div>
-        <div className="blackBody">
-
-          <div className="greyBox">
-            {dataRedo[this.state.dataDisplayIndex]}
-          </div>
-
-          <div className="buttonsToClick">
-            <button className="Previous" onClick={this.handlePrevious}>Previous</button>
-            <div className="EditDeleteNew">
-              <button className='Edit'>Edit</button>
-              <button className='Delete'>Delete</button>
-              <button className='New'>New</button>
+            <div className="greyBox">
+              {dataRedo[this.state.dataDisplayIndex]}
             </div>
-            <button className='Next'>Next</button>
+
+            <div className="buttonsToClick">
+              <button className="Previous" onClick={this.handlePrevious}>Previous</button>
+              <div className="EditDeleteNew">
+                <button className='Edit'>Edit</button>
+                <button className='Delete'>Delete</button>
+                <button className='New'>New</button>
+              </div>
+              <button className='Next'>Next</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
 }
 
 
